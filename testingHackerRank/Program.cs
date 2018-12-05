985d1774572c5e86d27cb17f6c88a7fb54412466
RehanZ <bc140201192@vu.edu.pk>
2018-12-06 01:49:51 +0500

    Day 1 Challange

--- testingHackerRank/Program.cs
+++ testingHackerRank/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,29 @@ namespace testingHackerRank
     {
         static void Main(string[] args)
         {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+
+            // Declare second integer, double, and String variables.
+            int a;
+            double b;
+            string c;
+            // Read and save an integer, double, and String to your variables.
+            a= Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToDouble(Console.ReadLine());
+            c=Console.ReadLine();
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i+a);
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(String.Format("{0:0.0}", d + b));
+            // Concatenate and print the String variables on a new line
+            Console.WriteLine(s + c);
+            // The 's' variable above should be printed first.
+            Console.ReadLine();
+
         }
     }
 }
